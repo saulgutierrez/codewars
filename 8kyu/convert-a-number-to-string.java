033858b96d95a1edb784b46fce23dd7bdde17bdb
Saúl Gutiérrez <sauladai@hotmail.com>
2023-02-01 14:00:27 -0600

    Added some kata

---
+++ 8kyu/convert-a-number-to-string.java
@@ -0,0 +1,12 @@
+// Description:
+// We need a function that can transform a number (integer) into a string.
+// What ways of achieving this do you know?
+
+// My solution:
+class Kata {
+  public static String numberToString(int num) {
+    // Return a string of the number here!
+    String number = String.valueOf(num);
+    return number;
+  }
+}
