f549b2474205c826226f3ccfa51bcd509b569792
Saúl Gutiérrez <sauladai@hotmail.com>
2023-04-07 00:16:44 -0600

    Add a Java kata

---
+++ 8kyu/sum-of-positive.java
@@ -0,0 +1,19 @@
+/**
+ * Description:
+ * You get an array of numbers, return the sum of all of the positives ones.
+ * Example [1,-4,7,12] => 1 + 7 + 12 = 20
+ * Note: if there is nothing to sum, the sum is default to 0.
+ */
+
+// My solution:
+public class Positive{
+  public static int sum(int[] arr){
+    int sum = 0;
+    for(int i = 0; i < arr.length; i++) {
+      if(arr[i] >= 0) {
+        sum += arr[i];
+      }
+    }
+    return sum;
+  }
+}
